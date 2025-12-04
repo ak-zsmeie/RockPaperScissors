43ae5d4b614dd1774302db3256ad23c914395afd
ak-zsmeie <adrian.krakowiak@zsmeie.edu.torun.pl>
2025-12-04 10:10:33 +0100

    Update Gra.java

--- RockPaperScissors/src/main/java/org/example/Gra.java
+++ RockPaperScissors/src/main/java/org/example/Gra.java
@@ -13,5 +13,35 @@ public class Gra {
         return 3;
     }
 
-}
+    public int wynikRundy(char opp, char me) {
+        if (opp == 'A') {
+            if (me == 'X') return 3;
+            if (me == 'Y') return 6;
+            return 0;
+        }
+        if (opp == 'B') {
+            if (me == 'X') return 0;
+            if (me == 'Y') return 3;
+            return 6;
+        }
+        if (opp == 'C') {
+            if (me == 'X') return 6;
+            if (me == 'Y') return 0;
+            return 3;
+        }
+        return 0;
+    }
 
+    public int policzWiersz(String w) {
+        char[] z = zamienNapis(w);
+        int figura = figuraGracza(z[1]);
+        int wynik = wynikRundy(z[0], z[1]);
+        return figura + wynik;
+    }
+
+    public int policzCalaGre(String[] dane) {
+        int suma = 0;
+        for (String d : dane) suma += policzWiersz(d);
+        return suma;
+    }
+}
